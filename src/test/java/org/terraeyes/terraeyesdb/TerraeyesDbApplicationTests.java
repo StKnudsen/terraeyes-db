d872e9b206b7fad522a0925fd04302797cf0db45
StKnudsen <70377462+StKnudsen@users.noreply.github.com>
2022-05-18 11:16:14 +0200

    Initial commit

---
+++ src/test/java/org/terraeyes/terraeyesdb/TerraeyesDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.terraeyes.terraeyesdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class TerraeyesDbApplicationTests
+{
+
+  @Test void contextLoads()
+  {
+  }
+
+}
