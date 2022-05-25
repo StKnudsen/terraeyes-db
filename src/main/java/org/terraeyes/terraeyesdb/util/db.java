fc0ccf05a444c7bf3ea7f8813e750c2210214085
StKnudsen <70377462+StKnudsen@users.noreply.github.com>
2022-05-25 14:56:38 +0200

    util db

--- src/main/java/org/terraeyes/terraeyesdb/util/db.java
+++ src/main/java/org/terraeyes/terraeyesdb/util/db.java
@@ -2,6 +2,6 @@ package org.terraeyes.terraeyesdb.util;
 
 public class db
 {
-  public final static String USER     = "postgres";
+  public final static String USER     = "user";
   public final static String PASSWORD = "KrqPWCJzs87HgKW4";
 }
