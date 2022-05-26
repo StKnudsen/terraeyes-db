d91153bb07bb7cdcc45f44844f4ed717a5ad0fb5
StKnudsen <70377462+StKnudsen@users.noreply.github.com>
2022-05-26 18:40:50 +0200

    Testing .secrets and .getenv

--- src/main/java/org/terraeyes/terraeyesdb/util/db.java
+++ src/main/java/org/terraeyes/terraeyesdb/util/db.java
@@ -2,6 +2,8 @@ package org.terraeyes.terraeyesdb.util;
 
 public class db
 {
-  public final static String USER     = "postgres";
-  public final static String PASSWORD = "KrqPWCJzs87HgKW4";
+  //public final static String USER     = "postgres";
+  public final static String USER     = System.getenv("ENV_USER");
+  //public final static String PASSWORD = "KrqPWCJzs87HgKW4";
+  public final static String PASSWORD = System.getenv("ENV_PASSWORD");
 }
